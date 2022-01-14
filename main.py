302300ea28b3d604db890477c85235d2df0f2108
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-01-14 14:17:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from matplotlib import rcParams, cycler
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+N = 10
+data = (np.geomspace(1, 10, 100) + np.random.randn(N, 100)).T
+cmap = plt.cm.coolwarm
+rcParams['axes.prop_cycle'] = cycler(color=cmap(np.linspace(0, 1, N)))
+
+fig, ax = plt.subplots()
+lines = ax.plot(data)
+ax.legend()
+from matplotlib.lines import Line2D
+custom_lines = [Line2D([0], [0], color=cmap(0.), lw=4),
+                Line2D([0], [0], color=cmap(.5), lw=4),
+                Line2D([0], [0], color=cmap(1.), lw=4)]
+
+fig, ax = plt.subplots()
+lines = ax.plot(data)
+ax.legend(custom_lines, ['Cold', 'Medium', 'Hot'])
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+
+legend_elements = [Line2D([0], [0], color='b', lw=4, label='Line'),
+                   Line2D([0], [0], marker='o', color='w', label='Scatter',
+                          markerfacecolor='g', markersize=15),
+                   Patch(facecolor='orange', edgecolor='r',
+                         label='Color Patch')]
+
+# Create the figure
+fig, ax = plt.subplots()
+ax.legend(handles=legend_elements, loc='center')
+
+plt.show()
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+
+legend_elements = [Line2D([0], [0], color='b', lw=4, label='Line'),
+                   Line2D([0], [0], marker='o', color='w', label='Scatter',
+                          markerfacecolor='g', markersize=15),
+                   Patch(facecolor='orange', edgecolor='r',
+                         label='Color Patch')]
+
+# Create the figure
+fig, ax = plt.subplots()
+ax.legend(handles=legend_elements, loc='center')
+
+plt.show()
